86aafc6fd453198dc7503ca9723a483446dcc4e7
David Harrison <djrh777@gmail.com>
2015-07-05 12:39:04 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# purescript-functor-compose
+
+[![Build Status](https://travis-ci.org/DavidHarrison/purescript-functor-compose.svg?branch=master)](https://travis-ci.org/DavidHarrison/purescript-functor-compose)
+
+Composition of functors.
+
+## Installation
+
+```
+bower install "https://github.com/DavidHarrison/purescript-functor-compose.git"
+```
+
+## Module documentation
+
+- [Data.Functor.Compose](docs/Data/Functor/Compose.md)
---
+++ src/Data/Functor/Compose.purs
@@ -0,0 +1,58 @@
+-- | Based on the [Haskell module](http://hackage.haskell.org/package/transformers-0.4.3.0/docs/Data-Functor-Compose.html)
+module Data.Functor.Compose (Compose(Compose), decompose) where
+
+import Prelude
+
+import Control.Alt         (Alt, alt)
+import Control.Plus        (Plus, empty)
+import Control.Alternative (Alternative)
+import Control.Apply       (lift2)
+import Data.Foldable       (Foldable, foldl, foldMap, foldr)
+import Data.Traversable    (Traversable, sequence, traverse)
+
+data Compose f g a = Compose (f (g a))
+
+decompose :: forall f g a. (Compose f g) a -> f (g a)
+decompose (Compose x) = x
+
+instance showCompose :: (Show (f (g a))) => Show (Compose f g a) where
+    show = show <<< decompose
+
+instance eqCompose :: (Eq (f (g a))) => Eq (Compose f g a) where
+    eq (Compose a) (Compose b) = a == b
+
+instance ordCompose :: (Ord (f (g a))) => Ord (Compose f g a) where
+    compare (Compose a) (Compose b) = compare a b
+
+instance functorCompose :: (Functor f, Functor g)
+                        => Functor (Compose f g) where
+    -- map :: forall a b. (a -> b) -> (Compose f g) a -> (Compose f g) b
+    map f = Compose <<< map (map f) <<< decompose
+
+instance applyCompose :: (Apply f, Apply g) => Apply (Compose f g) where
+    apply (Compose f) (Compose x) = Compose $ apply <$> f <*> x
+
+instance applicativeCompose :: (Applicative f, Applicative g)
+                            => Applicative (Compose f g) where
+    pure = Compose <<< pure <<< pure
+
+instance foldableCompose :: (Foldable f, Foldable g)
+                         => Foldable (Compose f g) where
+    foldr f i = foldr   (flip (foldr f)) i <<< decompose
+    foldl f i = foldl   (foldl f)        i <<< decompose
+    foldMap f = foldMap (foldMap f)        <<< decompose
+
+instance traversableCompose :: (Traversable f, Traversable g)
+                            => Traversable (Compose f g) where
+    traverse f = map Compose <<< traverse (traverse f) <<< decompose
+    sequence   = traverse id
+
+-- Takes on the Alt instance of the outer Functor
+instance altCompose :: (Alt f, Functor g) => Alt (Compose f g) where
+    alt (Compose a) (Compose b) = Compose $ alt a b
+
+instance plusCompose :: (Plus f, Functor g) => Plus (Compose f g) where
+    empty = Compose empty
+
+instance alternativeCompose :: (Alternative f, Applicative g)
+                            => Alternative (Compose f g)
